51e124ed5a50dde655f25d94fb772501d310f9bd
JTMurray <60299252+JTMurrayTAH@users.noreply.github.com>
2026-06-05 15:44:30 +0100

    Inital Code Structure

---
+++ Source/DR/Public/GameMode/Cpp_DrGameState.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "Cpp_DrGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DR_API ACpp_DrGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+};
---
+++ Source/DR/Public/Interfaces/iInteractable.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "iInteractable.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UiInteractable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * General Interface for interaction.
+ */
+class DR_API IiInteractable
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category="Interaction")
+	void TryToInteract(AActor* Interactor);
+	
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category="Interaction")
+	void Successful_Interactor(AActor* Interactor);
+	
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category="Interaction")
+	void Failed_Interact(AActor* Interactor);
+};
